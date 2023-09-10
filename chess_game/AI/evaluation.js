2d374acdec48e511c0a5df892cc90a4aff75c54a
Muktadul Islam <76801029+MuktadulIslam@users.noreply.github.com>
2023-09-10 22:09:18 +0600

    now player can play inn tarminal

--- chess_game/AI/evaluation.js
+++ chess_game/AI/evaluation.js
@@ -8,22 +8,6 @@
 //     50, 50, 50, 50, 50, 50, 50, 50,
 //     0, 0, 0, 0, 0, 0, 0, 0
 // ]
-// const pawnEvalCom = [
-//     [0, 0, 0, 0, 0],
-//     [5, 10, -20, 10, 5],
-//     [-5, 5, 0, 0, -5],
-//     [5, 10, 25, 10, 5],
-//     [30, 30, 30, 30, 30],
-//     [50, 50, 50, 50, 50]
-// ]
-// const pawnEvalPlayer = [
-//     [50, 50, 50, 50, 50],
-//     [30, 30, 30, 30, 30],
-//     [5, 10, 25, 10, 5],
-//     [-5, 5, 0, 0, -5],
-//     [5, 10, -20, 10, 5],
-//     [0, 0, 0, 0, 0]
-// ]
 // knightEval = [
 //     -50, -40, -30, -30, -30, -30, -40, -50,
 //     -40, -20, 0, 0, 0, 0, -20, -40,
@@ -33,14 +17,6 @@
 //     -30, 5, 10, 15, 15, 10, 5, -30,
 //     -40, -20, 0, 5, 5, 0, -20, -40,
 //     -50, -40, -30, -30, -30, -30, -40, -50
-// ]
-// const knightEval = [
-//     [-40, -30, -30, -30, -40],
-//     [-20, 0, 0, 0, -20],
-//     [-30, 0, 10, 0, -30],
-//     [-30, 0, 10, 0, -30],
-//     [-20, 0, 0, 0, -20],
-//     [-40, -30, -30, -30, -40]
 // ]
 // bishopEvalWhite = [
 //     -20, -10, -10, -10, -10, -10, -10, -20,
@@ -87,129 +63,102 @@
 // const constant = require('../constant.js');
 // const check_pieces = require('../moveFunction/check_pieces.js');
 
-import * as constant from '../constant.js';
-import * as check_pieces from '../moveFunction/check_pieces.js';
+import * as constant from '../constant.js'
+import * as  check_pieces from '../moveFunction/check_pieces.js'
 
+// Those are an other evalution position 
 // priority of killing
-const KING_VALUE = 20000;
-const QUEEN_VALUE = 900;
-const ROOK_VALUE = 500;
-const BISHOP_VALUE = 330;
-const KNIGHT_VALUE = 320;
-const PAWN_VALUE = 100;
-
-
+const KING_VALUE = 0;
+const QUEEN_VALUE = 9;
+const ROOK_VALUE = 5;
+const BISHOP_VALUE = 4;
+const KNIGHT_VALUE = 5;
+const PAWN_VALUE = 1;
 
 const pawnEvalCom = [
     [0, 0, 0, 0, 0],
-    [5, 5, 5, 5, 5],
-    [2, 10, 10, 12, 5],
-    [10, 20, 20, 20, 10],
-    [50, 50, 50, 50, 50],
-    [0, 0, 0, 0, 0]
+    [1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 1],
+    [2, 3, 3, 3, 2],
+    [3, 4, 4, 4, 3],
+    [5, 5, 5, 5, 5]
 ]
+
 const pawnEvalPlayer = [
-    [0, 0, 0, 0, 0],
-    [50, 50, 50, 50, 50],
-    [10, 20, 20, 20, 10],
-    [2, 10, 10, 12, 5],
     [5, 5, 5, 5, 5],
+    [4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3],
+    [2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0]
 ]
 
 const knightEval = [
-    [2, 5, 7, 5, 2],
-    [5, 7, 10, 7, 5],
-    [5, 10, 12, 10, 5],
-    [5, 10, 12, 10, 5],
-    [5, 7, 10, 7, 5],
-    [2, 5, 7, 5, 2]
+    [1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 3, 2, 1],
+    [1, 2, 3, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1]
 ]
 
 const bishopEval = [
-    [5, 7, 10, 7, 5],
-    [7, 10, 15, 10, 7],
-    [10, 15, 20, 15, 10],
-    [10, 15, 20, 15, 10],
-    [7, 10, 15, 10, 7],
-    [5, 7, 10, 7, 5]
+    [3, 2, 1, 2, 3],
+    [3, 3, 2, 3, 3],
+    [2, 3, 3, 3, 2],
+    [2, 3, 3, 3, 2],
+    [3, 3, 2, 3, 3],
+    [3, 2, 1, 2, 3]
+]
+const rookEval = [
+    [3, 3, 3, 3, 3],
+    [3, 2, 2, 2, 3],
+    [1, 2, 1, 2, 1],
+    [1, 2, 1, 2, 1],
+    [3, 2, 2, 2, 3],
+    [3, 3, 3, 3, 3]
 ]
 
-const rookEvalCom = [
-    [0, 0, 5, 0, 0],
-    [-5, 5, 10, 5, -5],
-    [-5, 0, 0, 0, -5],
-    [-5, 0, 0, 0, -5],
-    [-5, 0, 0, 0, -5],
-    [0, 0, 5, 0, 0]
-];
-const rookEvalPlayer = [
-    [0, 0, 5, 0, 0],
-    [-5, 0, 0, 0, -5],
-    [-5, 0, 0, 0, -5],
-    [-5, 0, 0, 0, -5],
-    [-5, 5, 10, 5, -5],
-    [0, 0, 5, 0, 0],
-];
-
 const queenEval = [
-    [5, 10, 10, 10,  5],
-    [5, 12, 15, 12,  5],
-    [10, 15, 20, 15, 10],
-    [10, 15, 20, 15, 10],
-    [5, 12, 15, 12,  5],
-    [5, 10, 10, 10,  5]
-];
-
-const kingEvalCom = [
-    [1,  5, 10, 5, 1],
-    [2, 10, 0, 10, 2],
-    [3, 10, 10, 10, 3],
-    [2, 10, 10, 10, 2],
-    [1, 0, 5, 0, 1],
-    [0, 0, 1, 0, 0]
-];
-
-const kingEvalPlayer = [
-    [0, 0, 1, 0, 0],
-    [1, 0, 5, 0, 1],
-    [2, 10, 10, 10, 2],
-    [3, 10, 10, 10, 3],
-    [2, 10, 0, 10, 2],
-    [1,  5, 10, 5, 1]
+    [1, 2, 1, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 3, 2, 1],
+    [1, 2, 3, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 1, 2, 1]
 ]
 
 
 function getComputerPieceValue(piece, y, x) {
-    if(piece == constant.COMPUTER_KING) return kingEvalCom[y][x]*KING_VALUE;
-    else if(piece == constant.COMPUTER_QUEEN) return queenEval[y][x]*QUEEN_VALUE;
-    else if(piece == constant.COMPUTER_ROOK) return rookEvalCom[y][x]*ROOK_VALUE;
-    else if(piece == constant.COMPUTER_KNIGHT) return knightEval[y][x]*KNIGHT_VALUE;
-    else if(piece == constant.COMPUTER_BISHOP) return bishopEval[y][x]*BISHOP_VALUE;
-    else if(piece == constant.COMPUTER_PAWN) return pawnEvalCom[y][x]*PAWN_VALUE;
+    if (piece == constant.COMPUTER_KING) return 0;
+    else if (piece == constant.COMPUTER_QUEEN) return queenEval[y][x] * QUEEN_VALUE;
+    else if (piece == constant.COMPUTER_ROOK) return rookEval[y][x] * ROOK_VALUE;
+    else if (piece == constant.COMPUTER_KNIGHT) return knightEval[y][x] * KNIGHT_VALUE;
+    else if (piece == constant.COMPUTER_BISHOP) return bishopEval[y][x] * BISHOP_VALUE;
+    else if (piece == constant.COMPUTER_PAWN) return pawnEvalCom[y][x] * PAWN_VALUE;
     else return 0;
 }
 
 function getPlayerPieceValue(piece, y, x) {
-    if(piece == constant.PLAYER_KING) return kingEvalPlayer[y][x]*KING_VALUE;
-    else if(piece == constant.PLAYER_QUEEN) return queenEval[y][x]*QUEEN_VALUE;
-    else if(piece == constant.PLAYER_ROOK) return rookEvalPlayer[y][x]*ROOK_VALUE;
-    else if(piece == constant.PLAYER_KNIGHT) return knightEval[y][x]*KNIGHT_VALUE;
-    else if(piece == constant.PLAYER_BISHOP) return bishopEval[y][x]*BISHOP_VALUE;
-    else if(piece == constant.PLAYER_PAWN) return pawnEvalPlayer[y][x]*PAWN_VALUE;
+    if (piece == constant.PLAYER_KING) return 0;
+    else if (piece == constant.PLAYER_QUEEN) return queenEval[y][x] * QUEEN_VALUE;
+    else if (piece == constant.PLAYER_ROOK) return rookEval[y][x] * ROOK_VALUE;
+    else if (piece == constant.PLAYER_KNIGHT) return knightEval[y][x] * KNIGHT_VALUE;
+    else if (piece == constant.PLAYER_BISHOP) return bishopEval[y][x] * BISHOP_VALUE;
+    else if (piece == constant.PLAYER_PAWN) return pawnEvalPlayer[y][x] * PAWN_VALUE;
     else return 0;
 }
 
 
 function evaluateBoard(board) {
     totalValue = 0;
-    for(let i=0 ; i<constant.BOARD_LENGTH ; i++) {
-        for(let j=0 ; j<constant.BOARD_WIDTH ; j++) {
-            if(board[i][j] == null) continue;
-            else if(check_pieces.isComputerPieces(board[i][j])) {
+    for (let i = 0; i < constant.BOARD_LENGTH; i++) {
+        for (let j = 0; j < constant.BOARD_WIDTH; j++) {
+            if (board[i][j] == null) continue;
+            else if (check_pieces.isComputerPieces(board[i][j])) {
                 totalValue += getComputerPieceValue(board[i][j], i, j);
             }
-            else if(check_pieces.isPlayerPieces(board[i][j])) {
+            else if (check_pieces.isPlayerPieces(board[i][j])) {
                 totalValue -= getPlayerPieceValue(board[i][j], i, j);
             }
         }
@@ -220,4 +169,4 @@ function evaluateBoard(board) {
 
 
 // module.exports = { evaluateBoard }
-export { evaluateBoard }
+export{ evaluateBoard }
---
+++ test.js
@@ -0,0 +1,160 @@
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+
+// const chess_game = require('./chess_game/chess_game.js');
+
+// const constant = chess_game.constant;
+// const board = chess_game.initializeBoard();
+// console.log("Let's Start the Game!!!!")
+// chess_game.printBoard(board);
+
+// while(true) {
+//     console.log("Enter an move:(-1 < x < 5    &   -1 < y < 6)");
+//     let cx,cy, nx, ny;
+
+//     // getting Player to select the pieces to move
+//     rl.question('Select y: ', (input) => {
+//         cy = parseInt(input);
+//         if (isNaN(integerInput)) {
+//           console.log('Invalid input. Please enter a valid integer.');
+//         }
+//         rl.close();
+//     });
+//     rl.question('Select  x: ', (input) => {
+//         cx = parseInt(input);
+//         if (isNaN(integerInput)) {
+//           console.log('Invalid input. Please enter a valid integer.');
+//         }
+//         rl.close();
+//     });
+
+//     // Player's all possible move for that piece
+//     console.log("Your possible moves are...",chess_game.getAllMovesForA_Position(board, cy, cx));
+
+//     // getting Player to select the next move
+//     rl.question('Next Move y: ', (input) => {
+//         ny = parseInt(input);
+//         if (isNaN(integerInput)) {
+//           console.log('Invalid input. Please enter a valid integer.');
+//         }
+//         rl.close();
+//     });
+//     rl.question('Next Move  x: ', (input) => {
+//         nx = parseInt(input);
+//         if (isNaN(integerInput)) {
+//           console.log('Invalid input. Please enter a valid integer.');
+//         }
+//         rl.close();
+//     });
+
+
+//     // Making the move
+//     if (board[cy][cx] == constant.PLAYER_PAWN && ny == 0) {
+//         // In UI Player will let to select the pieces to replace the pawn
+//         board[ny][nx] = constant.PLAYER_QUEEN;
+//         board[cy][cx] = null;
+//     }
+//     else {
+//         board[ny][nx] = board[cy][cx];
+//         board[cy][cx] = null;
+//     }
+//     console.log("After your moves the board will be...");
+//     chess_game.printBoard(board);
+
+
+
+//     // Now geting Computer moves
+//     let computerMove = chess_game.computersNextMove(board);
+//     cy = computerMove.currentPosition.y;
+//     cx = computerMove.currentPosition.x;
+//     ny = computerMove.nextPosition.y;
+//     nx = computerMove.nextPosition.x;
+
+//     if (board[cy][cx] == constant.COMPUTER_PAWN && ny == constant.BOARD_LENGTH-1) {
+//         board[ny][nx] = constant.COMPUTER_QUEEN;
+//         board[cy][cx] = null;
+//     }
+//     else {
+//         board[ny][nx] = board[cy][cx];
+//         board[cy][cx] = null;
+//     }
+
+//     console.log("After Computer's moves the board will be...");
+//     chess_game.printBoard(board);
+// }
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const chess_game = require('./chess_game/chess_game.js');
+const constant = chess_game.constant;
+const board = chess_game.initializeBoard();
+
+console.log("Let's Start the Game!!!!");
+chess_game.printBoard(board);
+
+function getPlayerInput(prompt, callback) {
+  rl.question(prompt, (input) => {
+    const value = parseInt(input);
+    if (isNaN(value)) {
+      console.log('Invalid input. Please enter a valid integer.');
+      getPlayerInput(prompt, callback); // Prompt again if input is invalid
+    } else {
+      callback(value);
+    }
+  });
+}
+
+function playGame() {
+  getPlayerInput('Select y: ', (cy) => {
+    getPlayerInput('Select x: ', (cx) => {
+      console.log("Your possible moves are...", chess_game.getAllMovesForA_Position(board, cy, cx));
+
+      getPlayerInput('Next Move y: ', (ny) => {
+        getPlayerInput('Next Move x: ', (nx) => {
+          if (board[cy][cx] === constant.PLAYER_PAWN && ny === 0) {
+            // Player will get opption to replace the pawn
+            board[ny][nx] = constant.PLAYER_QUEEN;
+          } else {
+            board[ny][nx] = board[cy][cx];
+          }
+          board[cy][cx] = null;
+
+          console.log("After your moves the board will be...");
+          chess_game.printBoard(board);
+
+          // Now get computer's moves
+          let computerMove = chess_game.computersNextMove(board);
+          cy = computerMove.currentPosition.y;
+          cx = computerMove.currentPosition.x;
+          ny = computerMove.nextPosition.y;
+          nx = computerMove.nextPosition.x;
+
+          if (board[cy][cx] === constant.COMPUTER_PAWN && ny === constant.BOARD_LENGTH - 1) {
+            board[ny][nx] = constant.COMPUTER_QUEEN;
+          } else {
+            board[ny][nx] = board[cy][cx];
+          }
+          board[cy][cx] = null;
+
+          console.log("After Computer's moves the board will be...");
+          chess_game.printBoard(board);
+
+          playGame(); // Continue the game
+        });
+      });
+    });
+  });
+}
+
+playGame(); // Start the game
